c80e497bf17ab5eac895ea63624e67e791eabc8e
woinshet1 <woinshet@DESKTOP-VDL1QKS.localdomain>
2022-04-03 04:48:44 -0400

    complet assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+    customerName = 'bob'.toUpperCase()
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer (name) {
+    bestCustomer = name
+}
+
+const leastFavoriteCustomer = 'another customer...';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Woinshet'
+}
